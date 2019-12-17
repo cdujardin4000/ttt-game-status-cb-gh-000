0c85d2a09d4d693c3976cd051e2b35ddd38577cd
Cedric Dujardin <cedric.dujardin@laposte.net>
2019-12-17 21:22:21 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,7 +35,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) == true || draw?(board) == true || draw?(board) == true
+  if  draw?(board) == true || draw?(board) == true
     return true
   end
 end
