91d85e41c9ba4e912f3c5beb3d5131eda5ff775b
Cedric Dujardin <cedric.dujardin@laposte.net>
2019-12-17 20:33:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,10 +32,4 @@ def draw?(board)
     return true
 end
 
-def valid_move?(board, index)
-  if position_taken?(board, index) == false && index.between?(0, 8)
-    return true
-  else
-    return false
-  end
-end
+
