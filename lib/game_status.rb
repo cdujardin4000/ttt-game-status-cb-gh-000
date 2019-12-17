da65007fc26c07836a2e491a1d19d54ad5310718
Cedric Dujardin <cedric.dujardin@laposte.net>
2019-12-17 16:51:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,13 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  
+  [7,8,9], 
+  [1,4,7],
+  [2,5,8], 
+  [3,6,9],
+  [1,5,9], 
+  [3,5,7]
+]
