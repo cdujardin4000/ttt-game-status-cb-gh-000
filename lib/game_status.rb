e41538d24a6a81f3feb4eb414703a86790183390
Cedric Dujardin <cedric.dujardin@laposte.net>
2019-12-17 19:12:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,11 +16,11 @@ WIN_COMBINATIONS = [
 ]
 
 def game_won?(board) WIN_COMBINATIONS.each do |wincombo|
-  wincombo = []
+
   if (board[wincombo[0]]) == "X" && (board[wincombo[1]]) == "X" && (board[wincombo[2]]) == "X"
-    return [wincombo]
+    return wincombo
   elsif (board[wincombo[0]]) == "O" && (board[wincombo[1]]) == "O" && (board[wincombo[2]]) == "O"
-    return [wincombo]
+    return wincombo
   end
 end
 end
