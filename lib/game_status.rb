2b518f839882b1a45c9f8b89471e7804ce5f1ce8
Cedric Dujardin <cedric.dujardin@laposte.net>
2019-12-17 18:52:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,7 @@ WIN_COMBINATIONS = [
 
 
 
-def game_status?(board)
+
 
     # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
     # grab each index from the win_combination that composes a win.
